46b23fa0575a5e70d25404d0871a41c81ac82bb5
Tajinder Singh <singhtajinder9888@gmail.com>
2026-08-16 22:39:07 +0530

    Updated prod backendurl

--- client/Tutty/src/Secrets/Secrets.js
+++ client/Tutty/src/Secrets/Secrets.js
@@ -3,11 +3,11 @@
 // export const SERVER_ADDRESS='https://api.tutty.tajinder.in'
 
 // /* eslint-disable no-unused-vars */
-export const SERVER_ADDRESS='http://localhost:3000'
+// export const SERVER_ADDRESS='http://localhost:3000'
 
 
 
 // *************   Cloudflared Tunnels ***********
-// export const SERVER_ADDRESS='https://apitutty.tajinder.in'
+export const SERVER_ADDRESS='https://apitutty.tajinder.in'
 
 
